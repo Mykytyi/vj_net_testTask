7a4a76240c9a3bb299171020e874793399934046
Nikita <cherevaty.nik@gmail.com>
2019-11-07 13:34:30 +0200

    Updates

---
+++ precache-manifest.b65bc952fe8cefa11f15496e53fafc58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9a98105192e4f0e2ddd9a14b0822f73",
+    "url": "/vj_net_testTask/index.html"
+  },
+  {
+    "revision": "bd1d0174718901cf250d",
+    "url": "/vj_net_testTask/static/css/main.09de2e61.chunk.css"
+  },
+  {
+    "revision": "9c04616fd11a4d5a12cd",
+    "url": "/vj_net_testTask/static/js/2.4bebc7df.chunk.js"
+  },
+  {
+    "revision": "bd1d0174718901cf250d",
+    "url": "/vj_net_testTask/static/js/main.bd6a8323.chunk.js"
+  },
+  {
+    "revision": "1d271179bb844e08770b",
+    "url": "/vj_net_testTask/static/js/runtime~main.830b1514.js"
+  }
+]);
